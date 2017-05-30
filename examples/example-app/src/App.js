6ba51f1f47279c99f61e214a3fd7d3b4a00f250d
tkjone <thomasmattacchione@gmail.com>
2017-05-30 17:30:32 -0400

    ISSUE #8 Add example project
    * Add example project
    
    * Improve README development quickstart

---
+++ examples/example-app/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { logInfo, logDebug, logWarn, logError } from 'pretty-logs';
+
+class App extends Component {
+  render() {
+    logInfo('CATEGORY', 'This is a message', {});
+    logDebug('CATEGORY', 'This is a message', {});
+    logWarn('CATEGORY', 'This is a message', {});
+    logError('CATEGORY', 'This is a message', {});
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
